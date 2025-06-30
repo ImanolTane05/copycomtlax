60d20fbd1e8e30e9d1f715b5467faed653c796bd
Derek Aguilar <aguilarrde753@gmail.com>
2025-06-29 21:00:02 -0600

    Pendiente - noticiaController.js y crearNoticias.js

--- backend/scripts/crearNoticias.js
+++ backend/scripts/crearNoticias.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 const Noticia = require('../models/Noticia');
 
 // TODO CREAR NOTICIAS
+mongoose.connect(process.env.MONGO_URI)
+    .then(async ()=> {
+        try {
+            // TODO
+        } catch (e) {
+            console.error("Error al insertar noticia: ",e);
+            process.exit(1);
+        }
+    })
